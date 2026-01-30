b86f502be32742a52a91883b74aeaf33c548d0aa
badadi2017-hue <badadi2017@gmail.com>
2026-01-30 20:02:30 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const cacheName = 'daily-calc-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.0/Sortable.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
